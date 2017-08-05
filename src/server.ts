77b71b779ed79dd539a3e24c53bfa1d4c17706ae
ThanhTDT <thanhtdt@outlook.com>
2017-08-05 11:00:04 +0700

    .

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+/**
+ * Module dependencies.
+ */
+import * as express from "express";
+import * as compression from "compression";  // compresses requests
+import * as bodyParser from "body-parser";
+import * as logger from "morgan";
+import * as errorHandler from "errorhandler";
+import * as dotenv from "dotenv";
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.config({ path: ".env.example" });
+
+/**
+ * Create Express server.
+ */
+const app = express();
+
+/**
+ * Express configuration.
+ */
+app.set("port", process.env.PORT || 3000);
+app.use(compression());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.end("Server is running");
+});
+
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+app.use(errorHandler());
+
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get("port"), () => {
+    console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
+    console.log("  Press CTRL-C to stop\n");
+});
+
+module.exports = app;
