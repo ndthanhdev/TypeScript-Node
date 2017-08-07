99e4396e5bb3318dfbaa3a184e65efc305efad9a
ThanhTDT <thanhtdt@outlook.com>
2017-08-07 10:38:03 +0700

    .

--- test/home.test.ts
+++ test/home.test.ts
@@ -5,6 +5,10 @@ const request = supertest("http://localhost:3000");
 describe("GET /", () => {
     it("should return 200 OK", (done) => {
         request.get("/")
-            .expect(200, done);
+            .expect(200).then(() => {
+                done();
+            }).catch((err) => {
+                done.fail(err);
+            });
     });
 });
